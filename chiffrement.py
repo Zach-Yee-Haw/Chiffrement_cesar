75b463ba374b1c3250b0ee40bb29820cea3f0a4a
Zach <zacharie.simoneau.1@ens.etsmtl.ca>
2025-01-30 15:10:13 -0500

    Ajout de commentaires

--- chiffrement.py
+++ chiffrement.py
@@ -1,4 +1,14 @@
+
 def Chiffrage_Cesar():
+
+    """
+    Cette fonction permet à l'utilisateur de prendre un message dans le fichier message_code.txt et de l'encrypter/décrypter
+    par le chiffrement de César.
+    """
+
+
+    #initialisation de variables
+
     nombre_lettres_alphabet = 26
     message_base = open("message_code.txt", "r", encoding='utf-8')
     message_base = message_base.read()
@@ -17,22 +27,34 @@ def Chiffrage_Cesar():
 
         message_code_liste = []
 
-        commande = input("Entrez un chiffre afin de coder/décoder le message, ou entrez \'d\' pour tenter de décoder le message : ")
+        #Entrée par l'utilisateur
+
+        commande = input("Entrez un chiffre afin de encrypter/décrypter le message, ou entrez \'d\' pour tenter de décoder le message : ")
+
+        #Si sa commande est un int ou 'd', on continue sinon c'est pas bien
 
         try:
             commande = int(commande)
         except ValueError:
             if commande != 'd': print("Commande invalide, essayez à nouveau.")
 
+        #On vérifie l'entrée de l'utilisateur, si c'est un 'd', on décode et si c'est un chiffre, on encrypte/décrypte
 
         if commande == "d":
             #
             print("test")
             #
         elif isinstance(commande, int):
-            #
+
+            """
+            On vérifie la position de la lettre dans l'alphabet, puis on lui ajoute la valeur de l'utilisateur.
+            On divise ensuite ce nombre par 26 et on prend le restant afin que ce chiffre corresponde à une lettre
+            dans l'alphabet. On ajoute cette lettre dans la liste du message codé. Si le charactère n'est pas une 
+            lettre, on l'ajoute directement dans la liste.
+            """
+
             i = 0
-            #
+
             for lettre in message_base_liste:
 
                 if lettre in case_haute_liste:
@@ -54,7 +76,11 @@ def Chiffrage_Cesar():
 
                 i += 1
 
+            #On unifie le message codé/décodé
+
             message_code = ''.join(message_code_liste)
+
+            #On montre le message à l'utilisateur
 
             print("Voici le message : ", message_code)
 
